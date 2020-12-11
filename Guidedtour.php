914cabecb7e1edf1e9aa482d268a109e112e9471
Kostas Mitropoulos <kmitrop@otenet.gr>
2020-12-11 09:27:26 +0200

    Update Guidedtour.php
    Queries for customer Moved to GuidedTourCustomer.php

--- Guidedtour.php
+++ Guidedtour.php
@@ -1,87 +1,13 @@
 <?php
     session_start();
-    //Connection to database
-    include 'Scripts/DbConnection.php';
-    
-    //To store current user tour start poi
-    $usersGuidedToursStartPoi = array();
-    //To store current user tour end poi
-    $usersGuidedToursEndPoi = array();
-    //Customer is connected
-    if (isset($_SESSION['currentCustomerId'])){
-        $CurrCustomerId=$_SESSION['currentCustomerId'];
-        //Find latest tour start and end pois for customer
-        //Find current tour Start Poi
-        $qrCurrentTourStartPoi = "SELECT pointsofinterest.PoiId as startPoiId, pointsofinterest.PoiName as startPoiName, pointsofinterest.PoiAddress as startPoiAddress, pointsofinterest.PoiLatitude as startPoiLatidute, pointsofinterest.PoiLongitude as startPoiLongitude, pointsofinterest.PoiBriefInfo as startPoiBriefInfo, pointsofinterest.PoiPhotoName as startPoiPhotoName,  pointsofinterest.PoiPhotoPath as startPoiPhotoPath  FROM pointsofinterest JOIN guidedtours ON pointsofinterest.PoiId = guidedtours.GuidedTourStartPoi WHERE guidedtours.GuidedTourCustomer = '$CurrCustomerId' ORDER BY GuidedTourDate DESC";
-        $qrCurrentTourStartPoiRes = mysqli_query($conn, $qrCurrentTourStartPoi);
-        $qrCurrentTourStartPoiRCount = mysqli_num_rows($qrCurrentTourStartPoiRes); 
-
-        //Insert records found to array
-        if ($qrCurrentTourStartPoiRCount!=0){
-            while($row = mysqli_fetch_assoc($qrCurrentTourStartPoiRes)){
-                $usersGuidedToursStartPoi[] = $row;
-            }
-        }
-        
-        //Find current tour End Poi
-        $qrCurrentTourEndPoi = "SELECT pointsofinterest.PoiId as endPoiId, pointsofinterest.PoiName as endPoiName, pointsofinterest.PoiAddress as endPoiAddress, pointsofinterest.PoiLatitude as endPoiLatidute, pointsofinterest.PoiLongitude as endPoiLongitude, pointsofinterest.PoiBriefInfo as endPoiBriefInfo, pointsofinterest.PoiPhotoName as endPoiPhotoName,  pointsofinterest.PoiPhotoPath as endPoiPhotoPath  FROM pointsofinterest JOIN guidedtours ON pointsofinterest.PoiId = guidedtours.GuidedTourEndPoi WHERE guidedtours.GuidedTourCustomer = '$CurrCustomerId' ORDER BY GuidedTourDate DESC";
-        $qrCurrentTourEndPoiRes = mysqli_query($conn, $qrCurrentTourEndPoi);
-        $qrCurrentTourEndPoiRCount = mysqli_num_rows($qrCurrentTourEndPoiRes); 
-
-        //Insert records found to array
-        if ($qrCurrentTourEndPoiRCount!=0){
-            while($row = mysqli_fetch_assoc($qrCurrentTourEndPoiRes)){
-                $usersGuidedToursEndPoi[] = $row;
-            }
-        }
-        
-        //Start End Pois coords
-        $startLat = $usersGuidedToursStartPoi[0]['startPoiLatidute'];
-        $startLng = $usersGuidedToursStartPoi[0]['startPoiLongitude'];
-        $endLat = $usersGuidedToursEndPoi[0]['endPoiLatidute'];
-        $endLng = $usersGuidedToursEndPoi[0]['endPoiLongitude'];
-        //Find all Other Pois
-        //Array to use
-        $allOtherPois = array();
-        //Start end pois
-        $foundStartPoiId = $usersGuidedToursStartPoi[0]['startPoiId'];
-        $foundEndPoiId = $usersGuidedToursEndPoi[0]['endPoiId'];
-        //Query for all Other Pois
-        $qrAllOtherPois = "SELECT * FROM pointsofinterest WHERE PoiId != '$foundStartPoiId' AND PoiId != '$foundEndPoiId'";
-        $qrAllOtherPoisRes = mysqli_query($conn, $qrAllOtherPois);
-        $qrAllOtherPoisRCount = mysqli_num_rows($qrAllOtherPoisRes);
-        
-        //Insert records found to array
-        if ($qrAllOtherPoisRCount!=0){
-            while ($row = mysqli_fetch_assoc($qrAllOtherPoisRes)){
-                if(checkPoi($startLat, $startLng, $endLat, $endLng, $row['PoiLatitude'],$row['PoiLongitude'])){
-                   $allOtherPois[]=$row; 
-                }
-            }
-        }
-           
+    //Check if customer, guide or driver connected
+    //If customer connected
+    if ($_SESSION['currentCustomerId']){
+        include 'Scripts/GuidedTourCustomer.php'; 
     }
-
     
-    mysqli_close($conn);
+    
 
-    function checkPoi($startLat, $startLng, $endLat, $endLng, $currLat, $currLng){
-        $accuracyValue = 0.001;
-        if(abs(($endLat-$startLat)*($currLng-$startLng)-($endLng-$startLng)*($currLat - $startLat))<$accuracyValue){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    //function checkPoi($startLat, $startLng, $endLat, $endLng, $currLat, $currLng){
-        //$accuracyValue = 0.001;
-        //if(abs(($endLat-$startLat)*($currLng-$startLng)-($endLng-$startLng)*($currLat - $startLat))<$accuracyValue){
-            //return true;
-        //}else{
-           // return false;
-        //}
-    //}
 ?>
 
 <html>
@@ -223,10 +149,12 @@
                     }
                     
                     //Creating the custom popup with bootstrap card for start poi
-                    var popupCardStartPoi = '<div class="card" style="width: 18rem;">'+
+                    var popupCardStartPoi = '<div class="card" style="width: 15rem; height: 30rem; ">'+
                                               '<img src="'+startPoiPhotoPath+startPoiPhotoName+'" class="card-img-top" alt="...">'+
-                                              '<div class="card-body">'+
-                                                '<h5 class="card-title">'+startPoiNm+'</h5>'+
+                                              '<div class="card-title">'+
+                                                '<h5>'+startPoiNm+'</h5>'+
+                                              '</div>'+
+                                              '<div class="card-body scroll" style="overflow-y: auto;">'+
                                                 '<p class="card-text">'+startPoiInf+'</p>'+
                                               '</div>'+
                                               '<ul class="list-group list-group-flush">'+
@@ -267,10 +195,12 @@
                     }
                     
                     //Creating the custom popup with bootstrap card for end poi
-                    var popupCardEndPoi = '<div class="card" style="width: 18rem;">'+
+                    var popupCardEndPoi = '<div class="card" style="width: 15rem; height: 30rem;">'+
                                               '<img src="'+endPoiPhotoPath+endPoiPhotoName+'" class="card-img-top" alt="...">'+
-                                              '<div class="card-body">'+
-                                                '<h5 class="card-title">'+endPoiNm+'</h5>'+
+                                              '<div class="card-title">'+
+                                                '<h5>'+endPoiNm+'</h5>'+
+                                              '</div>'+
+                                              '<div class="card-body scroll" style="overflow-y: auto;">'+
                                                 '<p class="card-text">'+startPoiInf+'</p>'+
                                               '</div>'+
                                               '<ul class="list-group list-group-flush">'+
@@ -288,7 +218,7 @@
                     addedmarker.bindTooltip("End", {permanent: true, direction: 'right'});
                     
                     //Add custom popup with the end poi info
-                    addedmarker.bindPopup(popupCardEndPoi).openPopup();
+                    addedmarker.bindPopup(popupCardEndPoi);
                     //End Poi marker--------------------------------------------------------------------------------------------------
                     
                     //Adding other markers--------------------------------------------------------------------------------------------
@@ -306,10 +236,12 @@
 								}
 							
 							 //Creating the custom popup with bootstrap card for end poi
-                            var popupCardOtherPoi = '<div class="card" style="width: 18rem;">'+
+                            var popupCardOtherPoi = '<div class="card" style="width: 15rem; height: 30rem; ">'+
                                               '<img src="'+poiPhotoPath+poiPhotoName+'" class="card-img-top" alt="...">'+
-                                              '<div class="card-body">'+
-                                                '<h5 class="card-title">'+poiNm+'</h5>'+
+                                              '<div class="card-title">'+
+                                                '<h5>'+poiNm+'</h5>'+
+                                              '</div>'+
+                                              '<div class="card-body scroll" style="overflow-y: auto;">'+
                                                 '<p class="card-text">'+poiInf+'</p>'+
                                               '</div>'+
                                               '<ul class="list-group list-group-flush">'+
@@ -323,8 +255,7 @@
 							addedmarker = new L.marker([poiLatd,poiLongd]).addTo(mymap);
 							
 							//Προσθήκη του castom popup και αντιστοίχιση των πληροφοριών
-							addedmarker.bindPopup(popupCardOtherPoi).openPopup();
-
+							addedmarker.bindPopup(popupCardOtherPoi);
 							
 				    }
                     //Adding other markers end--------------------------------------------------------------------------------------------
